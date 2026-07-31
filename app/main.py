c4d212582e309332c8b8e252261c33f663253f30
babu2004 <132599446+babu2004@users.noreply.github.com>
2026-07-31 17:40:59 +0000

    html connected to FastAPI

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 from app.schemas import QueryRequest, QueryResponse
 from rag.rag_pipeline import answer_question
+
+
 
 app = FastAPI(
     title = "RAG-API",
@@ -8,15 +14,26 @@ app = FastAPI(
     version = "1.0.0"
 )
 
-@app.get("/health")
-def health():
-    return {"status":"healthy"}
 
-@app.get("/")
-def root():
-    return{
-        "message":"welcome to the RAG API!"
-    }
+
+app.mount(
+    "/static",
+    StaticFiles(directory="app/static"),
+    name="static"
+)
+
+templates = Jinja2Templates(
+    directory="app/templates"
+)
+
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={}
+    )
 
 @app.post("/query",response_model=QueryResponse)
 def query(request: QueryRequest):
